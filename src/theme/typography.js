3441785557b1c3c49e700839ad9497b46d740bde
kelewang925-ship-it <kelewang925@gmail.com>
2026-05-25 13:57:32 +0800

    feat: add typography tokens

---
+++ src/theme/typography.js
@@ -0,0 +1,9 @@
+const typography = {
+  title: { fontSize: 28, lineHeight: 36 },
+  sectionTitle: { fontSize: 20, lineHeight: 28 },
+  subtitle: { fontSize: 16, lineHeight: 24 },
+  body: { fontSize: 14, lineHeight: 22 },
+  caption: { fontSize: 12, lineHeight: 18 },
+};
+
+export default typography;
