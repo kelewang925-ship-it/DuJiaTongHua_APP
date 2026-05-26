ba8e121e986fe464723c2fe8d7dd363726487d0a
kelewang925-ship-it <kelewang925@gmail.com>
2026-05-26 18:07:23 +0800

    phase-5: enable auth gate in root layout

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,12 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import AuthGate from '../src/components/AuthGate';
 
 export default function RootLayout() {
   return (
-    <>
+    <AuthGate>
       <StatusBar style="dark" />
       <Stack screenOptions={{ headerShown: false }} />
-    </>
+    </AuthGate>
   );
 }
